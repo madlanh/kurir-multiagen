529b99718746c60eda6859a2ab89816f4fab2394
madlanh <116224003+madlanh@users.noreply.github.com>
2025-05-28 20:22:49 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List, Tuple
 
 class Agent(BaseModel):
     id: str
@@ -14,3 +14,63 @@ class RegisterResponse(BaseModel):
     x: int
     y: int
     message: str
+
+class MoveRequest(BaseModel):
+    id: str
+    direction: str
+
+class MoveResponse(BaseModel):
+    id: str
+    x: int
+    y: int
+    message: str
+
+class Paket(BaseModel):
+    id: str
+    pickup_x: int
+    pickup_y: int
+    drop_x: int
+    drop_y: int
+    status: str
+    carried_by: Optional[str] = None
+
+class PaketActionRequest(BaseModel):
+    agent_id: str
+    action: str
+
+class PaketActionResponse(BaseModel):
+    message: str
+    paket_id: Optional[str] = None
+
+class AgentStatus(BaseModel):
+    id: str
+    x: int
+    y: int
+
+class PaketStatus(BaseModel):
+    id: str
+    pickup_x: int
+    pickup_y: int
+    drop_x: int
+    drop_y: int
+    status: str
+    carried_by: Optional[str] = None
+    
+class SystemStatus(BaseModel):
+    agents: List[AgentStatus]
+    pakets: List[PaketStatus]
+    obstacles: List[Tuple[int,int]]
+
+class AddPaketRequest(BaseModel):
+    id: str
+    pickup_x: int
+    pickup_y: int
+    drop_x: int
+    drop_y: int
+
+class AddPaketResponse(BaseModel):
+    message: str
+    paket_id: str
+
+class GridSizeRequest(BaseModel):
+    size: int
